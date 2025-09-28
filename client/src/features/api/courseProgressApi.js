866189c4288c0302853bb6ebdb86965282a7e58c
Rohit patidar <159720146+Rohitpatidar1@users.noreply.github.com>
2025-09-28 15:41:54 +0530

    Update courseProgressApi.js

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+// const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+const COURSE_PURCHASE_API = "https://learning-management-system-j7bb.onrender.com/api/v1/progress";
+
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
